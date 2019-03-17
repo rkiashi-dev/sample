1bd592e45ee11cda5776304fcb71560047634327
Toshiaki Kuroiwa <rkiashi@ToshiakinoMacBook-Air.local>
2019-03-17 15:09:20 +0900

    bootstrap

--- bootstrap/sandbox/b1/src/app/app.component.ts
+++ bootstrap/sandbox/b1/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+
+declare function $ ;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'b1';
+
+  ngOnInit() {
+
+    $(function () {
+  $('[data-toggle="popover"]').popover({
+    html: true,
+    trigger: 'click',
+    content: $('#cc')
+});
+
+ $('#bb').on('show.bs.popover',function(){
+   console.log("BBBB") ;
+   $('#cc').css( 'display', 'inherit' );
+   console.log( $('#cc') ) ;
+}) ; 
+
+})
+
+
+
+  }
+
+ click() {
+   alert('a');
+ }
+  
 }
