1f736760a402b6b0cb29312142b6c857bcd9fcfa
Toshiaki Kuroiwa <rkiashi@ToshiakinoMacBook-Air.local>
2019-03-31 08:31:54 +0900

    ruby regexp

--- ruby/regexp/m.rb
+++ ruby/regexp/m.rb
@@ -1,5 +1,8 @@
 
-r = Regexp.new( '[a-z\\200-\\377_]' ) 
+#r = Regexp.new( '[a-z\\200-\\377_]' ) 
+#r = Regexp.new( '[a-z_]'.force_encoding('UTF-8'),Regexp::FIXEDENCODING ) 
+r = Regexp.new( '[a-z_]' )
 p r.encoding
-'abc'.index( r, 1 ) 
-'/*  テスト */'.encode('US-ASCII').index( r, 1 ) 
+#'abc'.index( r, 1 ) 
+#'/*  テスト */'.encode('US-ASCII').index( r, 1 ) 
+p '/*  テスト */'.index( r, 1 ) 
