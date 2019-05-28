9470e83a0e06c88f2bdd637c195e49cd59dfa3de
rkiashi-dev <rkiashi-dev@gmail.com>
2019-05-29 01:26:11 +0900

    for rxjs

--- nodejs/rxjs/index.ts
+++ nodejs/rxjs/index.ts
@@ -8,12 +8,53 @@ import * as RxOpe from 'rxjs/operators';
 
 //const example =  Rx.interval(1000).pipe( RxOpe.takeWhile( val => val < 4 ) );
 
+/*
 Rx.concat(
  Rx.interval(1000).pipe( RxOpe.takeWhile( val => val < 4 ) ),
  Rx.interval(2000).pipe( RxOpe.takeWhile( val => val < 10 ) )
 ).
  pipe( RxOpe.takeWhile(val => val < 3 ))
  .subscribe( val => console.log( val ) ) ;
+*/
 
 //const subscribe = example.subscribe(val => console.log(val));
+
+function abc(): Rx.Observable<any> {
+  return new Rx.Observable<any>( (observer)=> {
+     console.log('next');
+     observer.next(true) ;
+      setTimeout( () => {
+     	 console.log('complete');
+         observer.complete() ;
+      },3000 ) ;
+  } ) ;
+}
+
+function eee(): Rx.Observable<any> {
+  return new Rx.Observable<any>( (observer)=> {
+     console.log('next');
+     observer.next(true) ;
+      setTimeout( () => {
+     	 console.log('err');
+         observer.error( 'eee' ) ;
+      },3000 ) ;
+  } ) ;
+}
+
+function zzz(): Rx.Observable<any> {
+  return new Rx.Observable<any>( (observer)=> {
+     console.log('next');
+     observer.next(true) ;
+  } ) ;
+}
+
+Rx.concat.apply( null, [
+  Rx.of(1, 2, 3).pipe(RxOpe.delay(3000)),
+  abc(),    // continue
+  //eee(),  // error 
+  //zzz(), // stop
+  Rx.of(4, 5, 6)
+  ]
+)
+  .subscribe(console.log);
 
